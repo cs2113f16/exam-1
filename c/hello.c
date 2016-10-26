d8a26481046597d950c7019d7c42392d4800b324
timwood <timwood@gwu.edu>
2016-10-25 20:46:55 -0400

    exam 1 base files

---
+++ c/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() 
+{
+  printf("Getting ready for exam 1... C works!\n");
+  return 0;
+}
---
+++ java/Hello.java
@@ -0,0 +1,7 @@
+
+public class Hello {
+  
+  public static void main(String[] args) {
+    System.out.println("Getting ready for exam 1... Java works!");
+  }
+}
